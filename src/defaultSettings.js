028b64fd94d67f9580b5fab35d7a0cf570341729
dengjiao <910392360@qq.com>
2020-08-09 22:53:31 +0800

    8999pp

---
+++ src/defaultSettings.js
@@ -0,0 +1,11 @@
+module.exports = {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  title: 'Ant Design Pro',
+  collapse: true,
+};
